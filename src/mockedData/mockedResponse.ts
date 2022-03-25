1e5ce7f3e11a785eec96c90fd9b8f358fc09201e
Andrius Vaitiekunas <waitiekunas@gmail.com>
2022-03-25 17:38:17 +0200

    Added test

---
+++ src/mockedData/mockedResponse.ts
@@ -0,0 +1,24 @@
+import { TableData } from '../types/api.types';
+
+export const mockedTableResponse: TableData[] = [
+  {
+    id: 1,
+    name: 'Jurgis'
+  },
+  {
+    id: 2,
+    name: 'Antanas'
+  },
+  {
+    id: 3,
+    name: 'Aloyzas'
+  },
+  {
+    id: 4,
+    name: 'Ir'
+  },
+  {
+    id: 5,
+    name: 'Martynas'
+  }
+];
---
+++ src/store/sagas/api.sagas.spec.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { expectSaga } from 'redux-saga-test-plan';
+import * as matchers from 'redux-saga-test-plan/matchers';
+
+import { setTableDataAction } from '../actions/api.actions';
+import { mockedTableResponse } from './../../mockedData/mockedResponse';
+import { mockedState } from './../../mockedData/mockedState';
+import { requestTableDataSaga } from './api.sagas';
+
+describe('requestTableDataSaga', ()=>{
+    it('should call api and set data', async ()=>{
+        return expectSaga(requestTableDataSaga)
+            .provide([[matchers.call.fn(axios.get), {data: mockedTableResponse}]])
+            .withState(mockedState)
+            .put(setTableDataAction(mockedTableResponse))
+            .run()
+    })
+})
